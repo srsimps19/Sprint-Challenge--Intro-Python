768c1113af4e5525d2697bfa1fdf49d8311a7c71
Sydney Crumley <srsimps19@gmail.com>
2020-01-17 10:05:11 -0600

    finished oop1.py about to test

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,46 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    def __init__(self):
+        #this is the base class
+        pass
+        
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+        #the base class is vehicle
+        pass
+
+class Car(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+        #the base class is ground vehicle
+        pass
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+        #the base class is ground vehicle
+        pass
+
+
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        super().__init__()
+        #the base class is vehicle
+        pass
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+        #the base class is flight vehicle
+        pass
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        super().__init__()
+        #the base class is flight vehicle
+        pass
